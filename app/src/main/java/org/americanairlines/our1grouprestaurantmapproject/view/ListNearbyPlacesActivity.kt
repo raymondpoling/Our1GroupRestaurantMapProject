6e6b067a82068ec3d1bd70eaf03d5ef291928df4
James <jh.jameshaisten@gmail.com>
2021-01-23 16:06:50 -0500

    added TODO from business requirements

--- app/src/main/java/org/americanairlines/our1grouprestaurantmapproject/view/ListNearbyPlacesActivity.kt
+++ app/src/main/java/org/americanairlines/our1grouprestaurantmapproject/view/ListNearbyPlacesActivity.kt
@@ -7,7 +7,13 @@ import org.americanairlines.our1grouprestaurantmapproject.R
 
 class ListNearbyPlacesActivity : AppCompatActivity() {
 
-
+//TODO Homework Weekend: Due 24th 11:59pm - To be presented in training
+//TODO Create an application that polls for the user location and lists all the nearby places in a recyclerView
+//TODO - application should use Kotlin
+//TODO - MVVM
+//TODO - Should cache using Room Database for offline usage
+//TODO - Application should use material design
+//TODO - Runtime permissions
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
