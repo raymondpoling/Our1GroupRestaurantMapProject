757a1e8d6dad0d076c44a8f623fda5d553c69e6e
James <jh.jameshaisten@gmail.com>
2021-01-23 15:24:22 -0500

    Created activity for list of places, and layouts for it

---
+++ app/src/main/java/org/americanairlines/our1grouprestaurantmapproject/ListNearbyPlacesActivity.kt
@@ -0,0 +1,24 @@
+package org.americanairlines.our1grouprestaurantmapproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class ListNearbyPlacesActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_nearby_places)
+
+
+
+
+    }// End of onCreate
+
+
+
+
+
+}// End of ListNearbyPlacesActivity
