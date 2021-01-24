1cab9edac5c8715a2240663cf1a5a57da5eb93e6
James <jh.jameshaisten@gmail.com>
2021-01-24 15:32:12 -0500

    Created Adapter

--- app/src/main/java/org/americanairlines/our1grouprestaurantmapproject/viewmodel/PlaceViewModel.kt
+++ app/src/main/java/org/americanairlines/our1grouprestaurantmapproject/viewmodel/PlaceViewModel.kt
@@ -5,13 +5,13 @@ import androidx.lifecycle.ViewModel
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
-import org.americanairlines.our1grouprestaurantmapproject.model.PlaceResult
+import org.americanairlines.our1grouprestaurantmapproject.model.googleapi.PlaceResult
 import org.americanairlines.our1grouprestaurantmapproject.network.PlaceRetrofit
 
 class PlaceViewModel: ViewModel() {
 
     val placeLiveData: MutableLiveData<List<PlaceResult>> = MutableLiveData()
-    private val placeRetrofit: PlaceRetrofit = PlaceRetrofit()
+    private val placeRetrofit: PlaceRetrofit = PlaceRetrofit
     private val compositeDisposable: CompositeDisposable = CompositeDisposable()
 
 //    fun getSearchResults(searchQueryPlace: String){
