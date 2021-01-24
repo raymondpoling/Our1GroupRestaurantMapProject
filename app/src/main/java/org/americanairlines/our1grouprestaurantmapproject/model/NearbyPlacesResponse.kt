f5768955b9619a08dc10b367ec6a1203b354c044
Raymond Poling <ruguerv@gmail.com>
2021-01-24 11:44:53 -0500

    Cleaning up my unnecessary additions

--- app/src/main/java/org/americanairlines/our1grouprestaurantmapproject/model/NearbyPlacesResponse.kt
+++
@@ -1,7 +0,0 @@
-package org.americanairlines.our1grouprestaurantmapproject.model
-
-data class NearbyPlacesResponse(
-    val html_attributions: List<Any>,
-    val results: List<Result>,
-    val status: String
-)
