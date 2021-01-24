8e562b69e44b0dce749e2d7eba7e8cb521e67036
Raymond Poling <ruguerv@gmail.com>
2021-01-24 10:27:28 -0500

    Interim commit to switch branches

--- app/src/main/java/org/americanairlines/our1grouprestaurantmapproject/network/PlaceRetrofit.kt
+++ app/src/main/java/org/americanairlines/our1grouprestaurantmapproject/network/PlaceRetrofit.kt
@@ -1,12 +1,39 @@
 package org.americanairlines.our1grouprestaurantmapproject.network
 
-import io.reactivex.disposables.Disposable
+import com.google.android.gms.maps.model.LatLng
+import io.reactivex.Observable
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.americanairlines.our1grouprestaurantmapproject.model.PlaceResponse
+import org.americanairlines.our1grouprestaurantmapproject.util.Constants.Companion.BASE_URL
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 
-class PlaceRetrofit {
-    fun getSearchQuery(s: String): String {
-        return "Place holder"
+
+object PlaceRetrofit {
+
+    private val okHttpClient = {
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
+        OkHttpClient.Builder().addInterceptor(interceptor).build()
+
+    }()
+
+    private val retrofit : Retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .client(okHttpClient)
+        .addConverterFactory(GsonConverterFactory.create())
+        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+        .build()
+
+    private val placesAPI : PlacesAPI = retrofit.create(PlacesAPI::class.java)
+
+    fun getNearbyPlaces(apiKey: String, location: LatLng, radius: Long) : Observable<PlaceResponse> {
+        return placesAPI.getNearbyPlaces(apiKey, location.paramString(), radius)
     }
 
-    //Place holder class
+    // extension function to define how LatLng is to be printed in calls
+    private fun LatLng.paramString() : String = "${this.latitude},${this.longitude}"
 
 }
--- app/src/main/java/org/americanairlines/our1grouprestaurantmapproject/network/PlacesAPI.kt
+++ app/src/main/java/org/americanairlines/our1grouprestaurantmapproject/network/PlacesAPI.kt
@@ -1,6 +1,18 @@
 package org.americanairlines.our1grouprestaurantmapproject.network
 
-class PlacesAPI {
+import io.reactivex.Observable
+import org.americanairlines.our1grouprestaurantmapproject.model.PlaceResponse
+import org.americanairlines.our1grouprestaurantmapproject.util.Constants.Companion.API_KEY
+import org.americanairlines.our1grouprestaurantmapproject.util.Constants.Companion.LOCATION
+import org.americanairlines.our1grouprestaurantmapproject.util.Constants.Companion.PLACES_PATH
+import org.americanairlines.our1grouprestaurantmapproject.util.Constants.Companion.RADIUS
+import retrofit2.http.GET
+import retrofit2.http.Query
 
-   // Place holder class
+interface PlacesAPI {
+
+    @GET(PLACES_PATH)
+    fun getNearbyPlaces(@Query(API_KEY) api_key : String,
+                       @Query(LOCATION) location:String,
+                       @Query(RADIUS) radius : Long = 500) : Observable<PlaceResponse>
 }
