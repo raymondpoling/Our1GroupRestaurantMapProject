5c8ba06ef0461873ec4292af3dba76151803514d
James <jh.jameshaisten@gmail.com>
2021-01-24 19:27:19 -0500

    Working on ViewModel, request permission from user activity

--- app/src/main/java/org/americanairlines/our1grouprestaurantmapproject/view/ListNearbyPlacesActivity.kt
+++ app/src/main/java/org/americanairlines/our1grouprestaurantmapproject/view/ListNearbyPlacesActivity.kt
@@ -1,17 +1,24 @@
 package org.americanairlines.our1grouprestaurantmapproject.view
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.gms.maps.model.LatLng
 import io.reactivex.Observer
 import org.americanairlines.our1grouprestaurantmapproject.R
+import org.americanairlines.our1grouprestaurantmapproject.model.googleapi.Geometry
 import org.americanairlines.our1grouprestaurantmapproject.model.googleapi.PlaceResult
 import org.americanairlines.our1grouprestaurantmapproject.view.adapter.PlaceAdapter
 import org.americanairlines.our1grouprestaurantmapproject.viewmodel.PlaceViewModel
 
 
-class ListNearbyPlacesActivity : AppCompatActivity() {
+class ListNearbyPlacesActivity : AppCompatActivity(), LocationListener {
 
 //TODO Homework Weekend: Due 24th 11:59pm - To be presented in training
 //TODO Create an application that polls for the user location and lists all the nearby places in a recyclerView
@@ -25,7 +32,7 @@ class ListNearbyPlacesActivity : AppCompatActivity() {
     private val viewModel: PlaceViewModel by viewModels()
     private lateinit var placeRecyclerView: RecyclerView
     private val placeAdapter: PlaceAdapter = PlaceAdapter(mutableListOf())
-
+    private lateinit var locationManager: LocationManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,5 +47,23 @@ class ListNearbyPlacesActivity : AppCompatActivity() {
 //        })
     }// End of onCreate
 
+    @SuppressLint("MissingPermission")
+    override fun onStart() {
+        super.onStart()
+
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000L, 2F, this)
+
+        var myLoc: LatLng = LatLng(33.74,84.38)
+
+        viewModel.getNearbyPlaces(myLoc)
+
+    }
+
+    override fun onLocationChanged(location: Location) {
+
+
+    }
 
 }// End of ListNearbyPlacesActivity
--- app/src/main/java/org/americanairlines/our1grouprestaurantmapproject/viewmodel/PlaceViewModel.kt
+++ app/src/main/java/org/americanairlines/our1grouprestaurantmapproject/viewmodel/PlaceViewModel.kt
@@ -1,22 +1,30 @@
 package org.americanairlines.our1grouprestaurantmapproject.viewmodel
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.android.gms.maps.model.LatLng
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
+import org.americanairlines.our1grouprestaurantmapproject.model.NearbyPlacesModel
 import org.americanairlines.our1grouprestaurantmapproject.model.googleapi.PlaceResult
 import org.americanairlines.our1grouprestaurantmapproject.network.PlaceRetrofit
+import org.americanairlines.our1grouprestaurantmapproject.repository.PlaceResultRepository
 
-class PlaceViewModel: ViewModel() {
+class PlaceViewModel : ViewModel() {
 
     val placeLiveData: MutableLiveData<List<PlaceResult>> = MutableLiveData()
     private val placeRetrofit: PlaceRetrofit = PlaceRetrofit
     private val compositeDisposable: CompositeDisposable = CompositeDisposable()
+    private val nearbyLiveData: MutableLiveData<List<NearbyPlacesModel>> = MutableLiveData()
+    private var placeResultRepository: PlaceResultRepository = PlaceResultRepository()
 
-    fun getSearchResults(searchQueryPlace: String){
 
+    fun getNearbyPlaces(location: LatLng): LiveData<List<NearbyPlacesModel>> {
 
+        placeResultRepository.getNearbyPlaces(location)
+        return nearbyLiveData
     }
 
     override fun onCleared() {
